a192ee5d78856009d5b51d34b6a122ee54df0860
vicat <vicat47@qq.com>
2023-05-27 01:16:03 +0800

    🔧：修改监听脚本，配置 bvid 和 redis

--- README.md
+++ README.md
@@ -12,6 +12,19 @@
 - obs
 - smb
 
+```bash
+perfect_bili_record_upload
+├─ README.md                        # README 文件
+├─ obs_scripts                      # OBS 脚本文件
+│    ├─ README.md                   ## 注意事项
+│    ├─ handle_stop.py              ## 监听录制停止事件，脚本的主要内容
+│    └─ load_config.py              ## 读取配置文件（测试）
+└─ win_scripts                      # windows 用的脚本
+       ├─ append_render_redis.bat   ## 将文件加入渲染列表
+       ├─ render_video.ps1          ## 渲染视频
+       └─ upload_video.ps1          ## 上传视频
+```
+
 ### 图解
 
 ![流程图](./README.assets/flow.drawio.svg)
@@ -19,6 +32,13 @@
 ## how
 
 1. 拖拽视频到 `append_render_redis.bat`。
-2. 打开 `render_video.ps1` 执行渲染视频命令。
-3. 打开 `upload_video.ps1` 等待视频上传。
-4. 打开 `bilibili`，欣赏你上传的影片。
+2. 使用 `OBS` 加载脚本：`handle_stop.py`
+3. 打开 `render_video.ps1` 执行渲染视频命令。
+4. 打开 `upload_video.ps1` 等待视频上传。
+5. 打开 `bilibili`，欣赏你上传的影片。
+
+
+# 参考文献
+
+1. [【bilibili】请叫我雯子小姐的小爷 | 69行代码！！！自己动手写一个OBS脚本插件，录制/直播任何你想展示的动态数据](https://www.bilibili.com/video/BV18t4y1S7by)
+2. [【github】upgradeQ | OBS Studio Python Scripting Cheatsheet](https://github.com/upgradeQ/OBS-Studio-Python-Scripting-Cheatsheet-obspython-Examples-of-API)
--- obs_scripts/handle_stop.py
+++ obs_scripts/handle_stop.py
@@ -5,11 +5,16 @@ from types import SimpleNamespace as _G
 from ctypes import *
 from ctypes.util import find_library
 import os
+import json
 
 
 G = _G()
 G.obsffi = CDLL(find_library("obs"))
 G.obsffi_front = CDLL(find_library("obs-frontend-api"))
+
+
+bvid = ''
+redis_config = {}
 
 
 def wrap(funcname, restype, argtypes=None, use_lib=None):
@@ -32,13 +37,6 @@ wrap("obs_frontend_get_profile_config", POINTER(Config), use_lib=G.obsffi_front)
 wrap("config_get_string", c_char_p, argtypes=[POINTER(Config), c_char_p, c_char_p])
 
 
-redis_config = {
-    "host": '192.168.31.15',
-    "port": 6379,
-    "database": 0
-}
-
-
 def get_rec_path() -> str:
     cfg = G.obs_frontend_get_profile_config()
     e = lambda x: x.encode("utf-8")
@@ -47,11 +45,11 @@ def get_rec_path() -> str:
 
 def get_recorded_file():
     base_path = Path(get_rec_path())
-    print(base_path)
     list = os.listdir(base_path)
     list.sort(key=lambda fileName: os.path.getmtime(base_path / fileName))
-    print(list[-1])
-    return base_path / list[-1]
+    file_path = base_path / list[-1]
+    print(f"recorded file in { file_path }")
+    return file_path
 
 
 def handle_rec_pause():
@@ -65,9 +63,9 @@ def handle_rec_stop():
 
 
 def rec_pause_callback(calldata):
-    print(calldata)
-    #out = S.calldata_ptr(calldata, "output") # bad type
     print('output paused')
+    # print(calldata)
+    #out = S.calldata_ptr(calldata, "output") # bad type
     r = redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_config['database'])
     r.set('obs:record', 'paused')
 
@@ -77,20 +75,73 @@ def rec_stop_callback(calldata):
     print('output stop')
     r = redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_config['database'])
     r.set('obs:record', 'stop')
-    r.xadd('stream-video-render', {'file': str(record_path)})
-    redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_config['database']).set('obs:record', 'stop')
+    json_str = json.dumps({
+        'filename': Path(record_path).name,
+        'bvid': bvid,
+    })
+    r.rpush('render-list', json_str)
+    print(f'render append {json_str}')
+    # r.xadd('stream-video-render', {'file': str(record_path)})
+    # redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_config['database']).set('obs:record', 'stop')
 
 
 def on_load(event):
     print("1 execute on load")
-    print(event)
+    # print(event)
     # if event == S.OBS_FRONTEND_EVENT_RECORDING_STARTED:
     #     handle_rec_pause()
 
 
 def script_load(settings):
+    global bvid
+    global redis_config
     print("0 script loaded")
     S.obs_frontend_add_event_callback(on_load)
+    # 启动时读取属性
+    bvid = S.obs_data_get_string(settings, "bvid")
+    redis_config['host'] = S.obs_data_get_string(settings, "redis_host")
+    redis_config['port'] = S.obs_data_get_int(settings, "redis_port")
+    redis_config['database'] = S.obs_data_get_int(settings, "redis_database")
+    print(f"loaded config bvid={ bvid }, redis_config={ str(redis_config) }")
     handle_rec_pause()
     handle_rec_stop()
     redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_config['database']).set('obs:status', 'load')
+
+
+def refresh_config(props, prop, settings):
+    global bvid
+    bvid_changed = S.obs_data_get_string(settings, "bvid")
+    redis_host_changed = S.obs_data_get_string(settings, "redis_host")
+    redis_port_changed = S.obs_data_get_int(settings, "redis_port")
+    redis_database_changed = S.obs_data_get_int(settings, "redis_database")
+    if bvid_changed != '': bvid = bvid_changed
+    if redis_host_changed != '': redis_config['host'] = redis_host_changed
+    if redis_port_changed != '': redis_config['port'] = redis_port_changed
+    if redis_database_changed != '': redis_config['database'] = redis_database_changed
+
+
+def script_properties():
+    props = S.obs_properties_create()
+    upload_bvid_input = S.obs_properties_add_text(
+        props, "bvid", "BVID will be upload", S.OBS_TEXT_DEFAULT
+    )
+    redis_host_input = S.obs_properties_add_text(
+        props, "redis_host", "redis host", S.OBS_TEXT_DEFAULT
+    )
+    redis_port_input = S.obs_properties_add_int(
+        props, "redis_port", "redis port", 1000, 65535, 1
+    )
+    redis_database_input = S.obs_properties_add_int(
+        props, "redis_database", "redis database", 0, 16, 1
+    )
+    S.obs_property_set_modified_callback(upload_bvid_input, refresh_config)
+    S.obs_property_set_modified_callback(redis_host_input, refresh_config)
+    S.obs_property_set_modified_callback(redis_port_input, refresh_config)
+    S.obs_property_set_modified_callback(redis_database_input, refresh_config)
+    return props
+
+
+def script_defaults(settings):
+    S.obs_data_set_default_string(settings, 'redis_host', '127.0.0.1')
+    S.obs_data_set_default_int(settings, 'redis_port', 6379)
+    S.obs_data_set_default_int(settings, 'redis_database', 0)
